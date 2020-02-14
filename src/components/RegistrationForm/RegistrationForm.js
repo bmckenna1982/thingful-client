d7bb5245e3b3c8f34611bfa14cbac96d296ee059
bmckenna1982 <bmckenna1982@gmail.com>
2020-02-14 11:28:15 -0500

    created auth-api-services and setup registration for to securely register new user

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -3,7 +3,7 @@ import { Button, Input, Required } from '../Utils/Utils'
 
 export default class RegistrationForm extends Component {
   static defaultProps = {
-    onRegistrationSuccess: () => {}
+    onRegistrationSuccess: () => { }
   }
 
   state = { error: null }
@@ -12,15 +12,27 @@ export default class RegistrationForm extends Component {
     ev.preventDefault()
     const { full_name, nick_name, user_name, password } = ev.target
 
-    console.log('registration form submitted')
-    console.log({ full_name, nick_name, user_name, password })
-
-    full_name.value = ''
-    nick_name.value = ''
-    user_name.value = ''
-    password.value = ''
-    this.props.onRegistrationSuccess()
+    // console.log('registration form submitted')
+    // console.log({ full_name, nick_name, user_name, password })
+    this.setState({ error: null })
+    AuthService.postUser({
+      full_name: full_name.value,
+      nickname: nick_name.value,
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(user => {
+        full_name.value = ''
+        nick_name.value = ''
+        user_name.value = ''
+        password.value = ''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
   }
+
 
   render() {
     const { error } = this.state
